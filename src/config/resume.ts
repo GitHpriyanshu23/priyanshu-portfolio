ec09506f2f2fc5454d53356acf97b9fce71018a1
GitHpriyanshu23 <priyanshuurmaliya23011@gmail.com>
2026-06-25 00:29:04 +0530

    Updated resume

--- src/config/resume.ts
+++ src/config/resume.ts
@@ -1,11 +1,11 @@
 export const resumeConfig = {
   title: "Resume",
   description: "A quick look at my experience, projects, and skills.",
-  fileId: "1CfeXXrfw-JrPvsCMX3TVfgOXN47UKbr0",
+  fileId: "18g3wGVYqM9IJBjCzp240VIDDVwYA1MiO",
   viewUrl:
-    "https://drive.google.com/file/d/1CfeXXrfw-JrPvsCMX3TVfgOXN47UKbr0/view?usp=sharing",
+    "https://drive.google.com/file/d/18g3wGVYqM9IJBjCzp240VIDDVwYA1MiO/view?usp=sharing",
   embedUrl:
-    "https://drive.google.com/file/d/1CfeXXrfw-JrPvsCMX3TVfgOXN47UKbr0/preview",
+    "https://drive.google.com/file/d/18g3wGVYqM9IJBjCzp240VIDDVwYA1MiO/preview",
   downloadUrl:
-    "https://drive.google.com/uc?export=download&id=1CfeXXrfw-JrPvsCMX3TVfgOXN47UKbr0",
+    "https://drive.google.com/uc?export=download&id=18g3wGVYqM9IJBjCzp240VIDDVwYA1MiO",
 };
